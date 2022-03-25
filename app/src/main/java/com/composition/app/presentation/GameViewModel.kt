7b4bc11c6bc241d61735cfc45d6a1f73ce433ace
Korsun Pavel <45067434+kkkkorsun@users.noreply.github.com>
2022-03-25 18:50:14 +0300

    Added GameViewModel.kt blank

---
+++ app/src/main/java/com/composition/app/presentation/GameViewModel.kt
@@ -0,0 +1,8 @@
+package com.composition.app.presentation
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class GameViewModel(application: Application) : AndroidViewModel(application) {
+
+}
