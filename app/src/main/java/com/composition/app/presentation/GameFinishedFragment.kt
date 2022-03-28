fc30a09ab707d8837e063ca1b1f6e4195c34005e
Korsun Pavel <45067434+kkkkorsun@users.noreply.github.com>
2022-03-28 19:43:40 +0300

    GameFinishedFragment.kt is showing info about game

--- app/src/main/java/com/composition/app/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/composition/app/presentation/GameFinishedFragment.kt
@@ -1,12 +1,14 @@
 package com.composition.app.presentation
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.composition.app.R
 import com.composition.app.databinding.FragmentGameFinishedBinding
 import com.composition.app.domain.entity.GameResult
 
@@ -34,17 +36,9 @@ class GameFinishedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        requireActivity().onBackPressedDispatcher.addCallback(
-            viewLifecycleOwner,
-            object : OnBackPressedCallback(true) {
-                override fun handleOnBackPressed() {
-                    retryGame()
-                }
-            })
+        setupClickListeners()
+        bindViews()
 
-        binding.buttonRetry.setOnClickListener {
-            retryGame()
-        }
     }
 
     override fun onDestroyView() {
@@ -52,9 +46,64 @@ class GameFinishedFragment : Fragment() {
         _binding = null
     }
 
+    private fun setupClickListeners() {
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                retryGame()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner, callback
+        )
+
+        binding.buttonRetry.setOnClickListener {
+            retryGame()
+        }
+
+    }
+
     private fun parseArgs() {
         requireArguments().getParcelable<GameResult>(GAME_RESULT)?.let {
             gameResult = it
+            Log.d("Score", it.toString())
+        }
+    }
+
+    private fun bindViews() {
+        with(binding) {
+            emojiResult.setImageResource(setImageResource())
+            tvRequiredAnswers.text = String.format(
+                getString(R.string.required_score),
+                gameResult.gameSettings.minCountOfRightAnswers
+            )
+            tvScoreAnswers.text = String.format(
+                getString(R.string.score_answers),
+                gameResult.countOfRightAnswers
+            )
+            tvRequiredPercentage.text = String.format(
+                getString(R.string.required_percentage),
+                gameResult.gameSettings.minPercentOfRightAnswers
+            )
+            tvScorePercentage.text = String.format(
+                getString(R.string.score_percentage),
+                getPercentOfRightAnswers()
+            )
+        }
+    }
+
+    private fun setImageResource(): Int {
+        return if (gameResult.winner) {
+            R.drawable.ic_win
+        } else {
+            R.drawable.ic_lose
+        }
+    }
+
+    private fun getPercentOfRightAnswers() = with(gameResult) {
+        if (countOfQuestions == 0) {
+            0
+        } else {
+            ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
         }
     }
 
