ca2d0d562453b8c1a964cf4f0aa63435e483f917
Korsun Pavel <45067434+kkkkorsun@users.noreply.github.com>
2022-04-01 17:05:53 +0300

    Second type to get args from another fragment with navArgs

--- app/src/main/java/com/composition/app/presentation/GameFragment.kt
+++ app/src/main/java/com/composition/app/presentation/GameFragment.kt
@@ -10,6 +10,7 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.composition.app.R
 import com.composition.app.databinding.FragmentGameBinding
 import com.composition.app.domain.entity.GameResult
@@ -18,8 +19,9 @@ import com.composition.app.domain.entity.Level
 
 class GameFragment : Fragment() {
 
+    private val args by navArgs<GameFragmentArgs>()
+
     private val viewModelFactory by lazy {
-        val args = GameFragmentArgs.fromBundle(requireArguments())
         GameViewModelFactory(args.level, requireActivity().application)
     }
     private val viewModel by lazy {
